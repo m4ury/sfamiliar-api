41d024b36871028189fec5cd2eacd6e6378cf1e3
m4ury <mauriciomorales0410@gmail.com>
2022-12-09 12:48:15 -0300

    add familiaController Resources y cambios menores en metodos de api

--- app/Http/Resources/PacienteResource.php
+++ app/Http/Resources/PacienteResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class PacienteResource extends JsonResource
 {
@@ -18,8 +19,9 @@ class PacienteResource extends JsonResource
             'id' => $this->id,
             'rut' => $this->rut,
             'ficha' => $this->ficha,
-            'nombres' => $this->nombres,
+            'nombres' => ucfirst($this->nombres ?? '').' '.ucfirst($this->apellidoP ?? '').' '.ucfirst($this->apellidoM ?? ''),
             'sector' => $this->sector,
+            'edad' => Carbon::parse($this->fecha_nacimiento)->age
         ];
     }
 }
